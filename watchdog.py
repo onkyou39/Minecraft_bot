d73a157bb0612164bee07fc0d6b7ca80350568bb
onkyou39 <as.1997.00@gmail.com>
2026-03-02 12:55:38 +0300

    Add server version check, code refactoring

--- watchdog.py
+++ watchdog.py
@@ -9,10 +9,15 @@ from dataclasses import dataclass, fields
 
 load_dotenv()
 
-SERVER_ADDRESS = os.getenv("SERVER_ADDRESS")  # или IP
-QUERY_PORT = 25565
-CHECK_INTERVAL = 60  # секунд между проверками
-WD_POWEROFF_COOLDOWN = 10 * 60 # 10 минут
+@dataclass
+class MinecraftServer:
+    server_address: str = os.getenv("SERVER_ADDRESS")  # или IP
+    query_port: int = 25565
+    check_interval: int = 60  # секунд между проверками
+    wd_poweroff_cooldown: int = 10 * 60  # 10 минут
+    version: str = ""
+
+MINECRAFT_SERVER = MinecraftServer()
 
 
 logging.basicConfig(
@@ -22,10 +27,10 @@ logging.basicConfig(
 
 
 logger = logging.getLogger("watchdog")
-logging.getLogger("watchdog").setLevel(logging.INFO)
+logger.setLevel(logging.INFO)
 
 async def get_players_list():
-    return await check_server_players(SERVER_ADDRESS, QUERY_PORT)
+    return await check_server_players()
 
 
 async def fast_check(host: str, port: int, timeout: float = 2.0):
@@ -45,7 +50,8 @@ async def fast_check(host: str, port: int, timeout: float = 2.0):
         logger.exception(f"Watchdog: port fast check — unknown exception: {e}")
         return None
 
-async def check_server_players(server_address: str, port: int):
+async def get_server_status(server_address: str = MINECRAFT_SERVER.server_address,
+                            port: int = MINECRAFT_SERVER.query_port):
     is_open = await fast_check(server_address, port, timeout=2)
     if is_open:
         try:
@@ -53,14 +59,27 @@ async def check_server_players(server_address: str, port: int):
             server = await JavaServer.async_lookup(f"{server_address}:{port}", timeout=3)
             logger.debug("Watchdog: mcstatus trying async_status...")
             status = await asyncio.wait_for(server.async_status(), timeout=6)
-            players_online = status.players.online
-            logger.debug(f"Watchdog: ONLINE {players_online} players online.")
-            return players_online
+            logger.debug(f"Watchdog: ONLINE Successfully get Minecraft server status.")
+            if not MINECRAFT_SERVER.version:
+                MINECRAFT_SERVER.version = status.version.name
+            return status
 
         except Exception as e:
             logger.debug(f"Watchdog: OFFLINE Minecraft server unreachable. {type(e).__name__}: {e}")
             return None
     else:
+        return None
+
+async def check_server_players():
+    status = await get_server_status()
+    if status:
+        # noinspection PyUnresolvedReferences
+        # для подавления warning в pycharm
+        players_online = status.players.online
+        logger.debug(f"Watchdog: ONLINE {players_online} players online.")
+        return players_online
+    else:
+        logger.debug(f"Watchdog: OFFLINE Failed to get list of players online.")
         return None
 
 @dataclass
@@ -74,61 +93,64 @@ class WatchdogState:
         for f in fields(self):
             setattr(self, f.name, f.default)
 
-watchdog_state = WatchdogState()
+WATCHDOG_STATE = WatchdogState()
 
 
 def reset_watchdog_state():
-    watchdog_state.reset()
+    WATCHDOG_STATE.reset()
 
 
 async def watchdog_tick(shutdown_callback, notify_callback=None):
     logger.debug("Watchdog tick.")
-    players = await check_server_players(SERVER_ADDRESS, QUERY_PORT)
+    players = await check_server_players()
     now = time.time()
 
     if players is not None:
-        watchdog_state.crashed = 0
-        if watchdog_state.is_fresh_start:
+        WATCHDOG_STATE.crashed = 0
+        if WATCHDOG_STATE.is_fresh_start:
             if notify_callback:
-                await notify_callback("✅ Minecraft сервер доступен для подключения.")
-            watchdog_state.is_fresh_start = False
+                await notify_callback(f"✅ Minecraft сервер доступен для подключения."
+                                      f"\nВерсия сервера: {MINECRAFT_SERVER.version}")
+            WATCHDOG_STATE.is_fresh_start = False
 
     if players == 0:
-        if watchdog_state.empty_since is None:
-            watchdog_state.empty_since = now
+        if WATCHDOG_STATE.empty_since is None:
+            WATCHDOG_STATE.empty_since = now
             logger.info("Watchdog: server is empty, starting shutdown timer")
-        elif now - watchdog_state.empty_since >= WD_POWEROFF_COOLDOWN:
+        elif now - WATCHDOG_STATE.empty_since >= MINECRAFT_SERVER.wd_poweroff_cooldown:
             logger.warning("Watchdog: server remained empty, cooldown passed — shutting down VPS")
             if notify_callback:
-                await notify_callback(f"🔴 На сервере не было игроков больше {WD_POWEROFF_COOLDOWN // 60} минут."
+                await notify_callback(f"🔴 На сервере не было игроков больше "
+                                      f"{MINECRAFT_SERVER.wd_poweroff_cooldown // 60} минут."
                                       f" Сервер выключен.")
             await shutdown_callback()
-            watchdog_state.empty_since = None  # Reset after shutdown
-            watchdog_state.notified = False  # сбрасываем флаг после выключения
-            watchdog_state.is_fresh_start = True # следующий запуск будет новым
+            WATCHDOG_STATE.empty_since = None  # Reset after shutdown
+            WATCHDOG_STATE.notified = False  # сбрасываем флаг после выключения
+            WATCHDOG_STATE.is_fresh_start = True # следующий запуск будет новым
         else:
-            remaining = int(WD_POWEROFF_COOLDOWN - (now - watchdog_state.empty_since))
+            remaining = int(MINECRAFT_SERVER.wd_poweroff_cooldown - (now - WATCHDOG_STATE.empty_since))
             logger.info(f"Watchdog: server still empty, {remaining} seconds left until shutdown")
-            if notify_callback and not watchdog_state.notified:
+            if notify_callback and not WATCHDOG_STATE.notified:
                 await notify_callback(f"ℹ️ На сервере нет игроков. "
-                                      f"Сервер будет выключен через {WD_POWEROFF_COOLDOWN // 60} минут.")
-                watchdog_state.notified = True  # для однократного вывода
+                                      f"Сервер будет выключен через "
+                                      f"{MINECRAFT_SERVER.wd_poweroff_cooldown // 60} минут.")
+                WATCHDOG_STATE.notified = True  # для однократного вывода
 
     elif players is not None:
-        if watchdog_state.empty_since is not None:
+        if WATCHDOG_STATE.empty_since is not None:
             logger.info("Watchdog: players joined — resetting shutdown timer")
-            watchdog_state.empty_since = None  # Reset timer because players are online
-            watchdog_state.notified = False
+            WATCHDOG_STATE.empty_since = None  # Reset timer because players are online
+            WATCHDOG_STATE.notified = False
     else: # случай с падением minecraft или первым запуском.
-        if not watchdog_state.is_fresh_start:
+        if not WATCHDOG_STATE.is_fresh_start:
             logger.warning("Watchdog: looks like minecraft server is crashed or unreachable.")
         else:
             logger.info("Watchdog: Minecraft server is offline and probably starting.")
-        if notify_callback and watchdog_state.crashed > 1 and not watchdog_state.is_fresh_start:
+        if notify_callback and WATCHDOG_STATE.crashed > 1 and not WATCHDOG_STATE.is_fresh_start:
             await notify_callback("⚠️ Minecraft сервер временно недоступен или аварийно завершил работу.")
-            watchdog_state.notified = False
-            watchdog_state.is_fresh_start = True # для вывода уведомления о запуске
-        elif notify_callback and watchdog_state.crashed == 0 and watchdog_state.is_fresh_start:
+            WATCHDOG_STATE.notified = False
+            WATCHDOG_STATE.is_fresh_start = True # для вывода уведомления о запуске
+        elif notify_callback and WATCHDOG_STATE.crashed == 0 and WATCHDOG_STATE.is_fresh_start:
             await notify_callback("⏳ Minecraft сервер запускается...")
-        watchdog_state.crashed += 1
-        watchdog_state.empty_since = None #  сброс таймера до корректного восстановления работы
+        WATCHDOG_STATE.crashed += 1
+        WATCHDOG_STATE.empty_since = None #  сброс таймера до корректного восстановления работы
